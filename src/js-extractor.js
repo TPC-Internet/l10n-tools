836b7c0592651eecd35d5173f0e9f7b6c20c50cb
Beren <beren.song@vonvon.me>
2018-03-10 14:28:05 +0900

    [JsExtractor] extract from interpolation code of template

--- src/js-extractor.js
+++ src/js-extractor.js
@@ -108,6 +108,29 @@ export class JsExtractor {
                     this.extractJsExpression(filename, content, line)
                 }
             }
+
+            for (const child of elem.children) {
+                if (child.type === 'text') {
+                    for (const marker of this.options.markers) {
+                        let data = child.data
+                        let dataIndex = 0
+                        while (data) {
+                            let startOffset = data.indexOf(marker.start)
+                            if (startOffset === -1)
+                                break
+
+                            startOffset += marker.start.length
+                            const endOffset = data.substr(startOffset).indexOf(marker.end)
+                            const content = data.substr(startOffset, endOffset)
+                            const line = getLineTo(src, child.startIndex + dataIndex + startOffset, startLine)
+                            this.extractJsExpression(filename, content, line)
+
+                            dataIndex += startOffset + endOffset + marker.end.length
+                            data = child.data.substr(dataIndex)
+                        }
+                    }
+                }
+            }
         })
     }
 
