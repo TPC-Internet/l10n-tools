a056b0d675ed00fca7afb309f1dab1b8a8b333a5
Beren <beren.song@vonvon.me>
2018-12-19 16:38:44 +0900

    i18next.js -> javascript.js again

--- src/extractor/index.js
+++ src/extractor/index.js
@@ -17,7 +17,7 @@ function loadExtractor (type) {
             return require('./vue-i18n').default
         case 'javascript':
         case 'i18next':
-            return require('./i18next').default
+            return require('./javascript').default
         case 'vt':
             return require('./vt').default
         case 'python':
