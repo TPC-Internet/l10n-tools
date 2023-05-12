10fd445b9b889dbd2a199a3605a1db8ea3a29a7e
Eungkyu Song <beren@tpcinternet.com>
2023-05-12 12:48:29 +0900

    dev: evenly request lokalise listing api

--- src/syncer/lokalise.ts
+++ src/syncer/lokalise.ts
@@ -39,7 +39,7 @@ async function listLokaliseKeys(lokaliseApi: LokaliseApi, projectId: string, con
     const totalCount = await getTotalKeyCount(lokaliseApi, projectId)
     const numPages = Math.ceil(totalCount / 500)
 
-    const queue = new PQueue({interval: 1000, intervalCap: 2})
+    const queue = new PQueue({interval: 500, intervalCap: 1})
     const chunkPromises: Promise<Key[]>[] = []
     for (let page = 1; page <= numPages; page++) {
         chunkPromises.push(queue.add(async () => {
