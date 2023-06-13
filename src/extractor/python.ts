79b5ebe3038af5845f697a9439c1830a4b9ca199
Eungkyu Song <beren@tpcinternet.com>
2023-06-14 02:08:35 +0900

    feat: disable python extractor

--- src/extractor/python.ts
+++ src/extractor/python.ts
@@ -3,6 +3,7 @@ import {getSrcPaths, xgettext} from '../common.js';
 import {type DomainConfig} from '../config.js';
 
 export default async function (domainName: string, config: DomainConfig, potPath: string) {
+    throw new Error('python extractor is not yet updated to new intermediate format')
     const srcPaths = await getSrcPaths(config, ['.py'])
     const keywords = config.getKeywords()
     log.info('extractPot', 'extracting from .py files')
