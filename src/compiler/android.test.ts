9fa0f2f6b79d8bd5e0816d8093be23faf689dc08
Eungkyu Song <beren@tpcinternet.com>
2023-06-16 14:37:33 +0900

    refactor: make android test utilize real code

--- src/compiler/android.test.ts
+++ src/compiler/android.test.ts
@@ -1,26 +1,46 @@
-import {
-    createCDataNode,
-    createTextNode,
-    findFirstTagNode,
-    getAndroidXmlBuilder,
-    getAndroidXmlParser,
-    getAttrValue,
-    isCDataNode,
-    isTagNode,
-    parseAndroidXml,
-    type XMLNode,
-    type XMLTagNode,
-} from './android-xml-utils.js';
+import type {TransEntry} from '../entry.js'
+import {generateAndroidXml} from './android.js'
 
 describe('android compiler test', () => {
-    it('parse and build preserving all', () => {
-        const transMap: {[name: string]: string} = {
-            sign_up_ask_comment_v2: '아직 계정이 없으신가요? <font color="#FF424D">회원가입</font>',
-            sign_up_form_policy_agreement_desc_v2: '<u>서비스 이용약관</u>과 <u>개인정보 처리방침</u>에 동의합니다.',
-            user_interest_form_title: '관심사 & 해시태그',
-            user_interest_edit_req_desc_for_fan: '<b>관심사 선택 기능이 새로 나왔습니다!</b>\\n관심사를 설정하시면 나와 맞는 크리에이터들을 추천해드릴께요!\\n아래에서 최소 3개 이상을 선택해주세요!',
-            alarm_list_item_desc_gift_dm_received: '<b>%1$s</b>님이 선물을 보냈습니다.'
-        }
+    it('parse and build preserving all', async () => {
+        const transEntries: TransEntry[] = [
+            {
+                context: 'sign_up_ask_comment_v2',
+                key: '아직 계정이 없으신가요? <font color="#FF424D">회원가입</font>',
+                messages: {other: 'Don\'t have an account? <font color="#FF424D">Sign up</font>'},
+                flag: null
+            },
+            {
+                context: 'sign_up_form_policy_agreement_desc_v2',
+                key: '<u>서비스 이용약관</u>과 <u>개인정보 처리방침</u>에 동의합니다.',
+                messages: {other: '<u>서비스 이용약관</u>과 <u>개인정보 처리방침</u>에 동의합니다.'},
+                flag: null
+            },
+            {
+                context: 'user_interest_form_title',
+                key: '관심사 & 해시태그',
+                messages: {other: '관심사 & 해시태그'},
+                flag: null
+            },
+            {
+                context: 'user_interest_edit_req_desc_for_fan',
+                key: '<b>관심사 선택 기능이 새로 나왔습니다!</b>\\n관심사를 설정하시면 나와 맞는 크리에이터들을 추천해드릴께요!\\n아래에서 최소 3개 이상을 선택해주세요!',
+                messages: {other: '<b>관심사 선택 기능이 새로 나왔습니다!</b>\\n관심사를 설정하시면 나와 맞는 크리에이터들을 추천해드릴께요!\\n아래에서 최소 3개 이상을 선택해주세요!'},
+                flag: null
+            },
+            {
+                context: 'alarm_list_item_desc_gift_dm_received',
+                key: '<b>%1$s</b>님이 선물을 보냈습니다.',
+                messages: {other: '<b>%1$s</b>님이 선물을 보냈습니다.'},
+                flag: null
+            },
+            {
+                context: 'text_left_days',
+                key: '%d days',
+                messages: {one: '%d day', other: '%d days'},
+                flag: null
+            }
+        ]
         const srcXml = `<?xml version="1.0" encoding="utf-8"?>
 <resources>
     <string name="app_name">LIKEY</string>
@@ -35,14 +55,12 @@ describe('android compiler test', () => {
         <item quantity="other">%d days</item>
     </plurals>
 </resources>`
-        const targetXml = `<?xml version="1.0" encoding="utf-8"?>
+        const dstXml = `<?xml version="1.0" encoding="utf-8"?>
 <resources>
-    
-    <string name="sign_up_ask_comment_v2" format="html">아직 계정이 없으신가요? <font color="#FF424D">회원가입</font></string>
+    <string name="sign_up_ask_comment_v2" format="html">Don't have an account? <font color="#FF424D">Sign up</font></string>
     <string name="sign_up_form_policy_agreement_desc_v2" format="html"><u>서비스 이용약관</u>과 <u>개인정보 처리방침</u>에 동의합니다.</string>
     <string name="user_interest_form_title"><![CDATA[관심사 & 해시태그]]></string>
     <string name="user_interest_edit_req_desc_for_fan" format="html"><b>관심사 선택 기능이 새로 나왔습니다!</b>\\n관심사를 설정하시면 나와 맞는 크리에이터들을 추천해드릴께요!\\n아래에서 최소 3개 이상을 선택해주세요!</string>
-    
     <string name="alarm_list_item_desc_gift_dm_received"><![CDATA[<b>%1$s</b>님이 선물을 보냈습니다.]]></string>
     <plurals name="text_left_days">
         <item quantity="one">%d day</item>
@@ -50,59 +68,7 @@ describe('android compiler test', () => {
     </plurals>
 </resources>`
 
-        const parser = getAndroidXmlParser()
-        const srcXmlJson = parseAndroidXml(parser, srcXml)
-
-        const resNode = findFirstTagNode(srcXmlJson, 'resources')
-        if (resNode == null) {
-            throw new Error('no resources tag')
-        }
-        const dstResources = resNode.resources
-            .map(node => {
-                // string 태그 외 text 영역, plurals 태그 등 다른 것은 그대로 복사
-                if (!isTagNode(node, 'string')) {
-                    return node
-                }
-
-                // translatable="false" 인 태그는 스킵
-                const translatable = getAttrValue(node, 'translatable')
-                if (translatable == 'false') {
-                    return null
-                }
-
-                // name attr 없는 태그는 문제가 있는 것인데, 일단 스킵
-                const name = getAttrValue(node, 'name')
-                if (name == null) {
-                    return null
-                }
-
-                // 번역이 없는 태그도 스킵
-                let value = transMap[name]
-                if (!value) {
-                    return null
-                }
-
-                // html format 은 번역 텍스트 그대로 사용
-                const format = getAttrValue(node, 'format')
-                if (format === 'html') {
-                    // no post process
-                    return {...node, string: [createTextNode(value, true)]} as XMLTagNode
-                } else {
-                    // CDATA 노드인 경우 CDATA를 그대로 살려서 스트링만 교체
-                    if (node.string.some(node => isCDataNode(node))) {
-                        return {...node, string: [createCDataNode(value, true)]}
-                    }
-
-                    // 그 외의 경우는 android string encoding 하여 사용
-                    return {...node, string: [createTextNode(value, false)]}
-                }
-            })
-            .filter((node): node is XMLNode => node != null)
-
-        resNode.resources = dstResources
-
-        const builder = getAndroidXmlBuilder()
-        let dstXml = builder.build(srcXmlJson)
-        expect(dstXml).toBe(targetXml)
+        const newDstXml = await generateAndroidXml('en', transEntries, srcXml, dstXml)
+        expect(newDstXml).toBe(dstXml)
     })
 })
--- src/compiler/android.ts
+++ src/compiler/android.ts
@@ -3,7 +3,7 @@ import log from 'npmlog'
 import shell from 'shelljs'
 import * as path from 'path'
 import {EntryCollection} from '../entry-collection.js'
-import {readTransEntries} from '../entry.js'
+import {readTransEntries, type TransEntry} from '../entry.js'
 import {type CompilerConfig} from '../config.js';
 import {
     buildAndroidXml,
@@ -22,7 +22,6 @@ import {
     type XMLTagNode,
     type XMLTextNode,
 } from './android-xml-utils.js';
-import type {XMLBuilder, XMLParser} from 'fast-xml-parser';
 import {extractLocaleFromTransPath, listTransPaths} from '../utils.js'
 
 export async function compileToAndroidXml(domainName: string, config: CompilerConfig, transDir: string) {
@@ -33,7 +32,8 @@ export async function compileToAndroidXml(domainName: string, config: CompilerCo
     const parser = getAndroidXmlParser()
     const builder = getAndroidXmlBuilder()
 
-    const srcXmlJson = await readXmlJson(parser, resDir, null)
+    const srcXml = await readXml(resDir, null)
+    const srcXmlJson = parseAndroidXml(parser, srcXml)
     const resNode = findFirstTagNode(srcXmlJson, 'resources')
     if (resNode == null) {
         throw new Error('no resources tag')
@@ -42,127 +42,142 @@ export async function compileToAndroidXml(domainName: string, config: CompilerCo
     const transPaths = await listTransPaths(transDir)
     for (const transPath of transPaths) {
         const locale = extractLocaleFromTransPath(transPath)
-        const dstXmlJson = await readXmlJson(parser, resDir, locale)
-        const dstResNode = findFirstTagNode(dstXmlJson, 'resources')
-        if (dstResNode == null) {
-            throw new Error('no resources tag')
+        if (locale === defaultLocale) {
+            await writeXml(buildAndroidXml(builder, srcXmlJson), resDir, null)
+        } else {
+            const transEntries = await readTransEntries(transPath)
+            const dstXml = await readXml(resDir, locale)
+            const newDstXml = await generateAndroidXml(locale, transEntries, srcXml, dstXml)
+            await writeXml(newDstXml, resDir, locale)
         }
+    }
+}
 
-        const trans = EntryCollection.loadEntries(await readTransEntries(transPath))
+export async function generateAndroidXml(locale: string, transEntries: TransEntry[], srcXml: string, dstXml: string) {
+    const parser = getAndroidXmlParser()
+    const builder = getAndroidXmlBuilder()
 
-        const dstResources: XMLNode[] = []
-        let passingText = false
-        for (const node of resNode.resources) {
-            if (isTextNode(node)) {
-                if (passingText) {
-                    passingText = false
-                } else {
-                    dstResources.push(node)
-                }
-                continue
-            }
+    const srcXmlJson = parseAndroidXml(parser, srcXml)
+    const resNode = findFirstTagNode(srcXmlJson, 'resources')
+    if (resNode == null) {
+        throw new Error('no resources tag')
+    }
 
-            // string 태그
-            if (isTagNode(node, 'string')) {
-                // translatable="false" 인 태그는 스킵
-                const translatable = getAttrValue(node, 'translatable')
-                if (translatable == 'false') {
-                    passingText = true
-                    continue
-                }
+    const dstXmlJson = parseAndroidXml(parser, dstXml)
+    const dstResNode = findFirstTagNode(dstXmlJson, 'resources')
+    if (dstResNode == null) {
+        throw new Error('no resources tag')
+    }
 
-                // name attr 없는 태그는 문제가 있는 것인데, 일단 스킵
-                const name = getAttrValue(node, 'name')
-                if (name == null) {
-                    passingText = true
-                    continue
-                }
+    const trans = EntryCollection.loadEntries(transEntries)
 
-                // 번역이 없는 태그도 스킵
-                const transEntry = trans.find(name, null)
-                if (transEntry == null) {
-                    passingText = true
-                    continue
-                }
-                let value = transEntry.messages.other
-                if (!value) {
-                    passingText = true
-                    continue
-                }
-
-                const valueNode = createValueNode(node, node.string, value)
-                dstResources.push({...node, string: [valueNode]})
-            } else if (isTagNode(node, 'plurals')) {
-                // translatable="false" 인 태그는 스킵
-                const translatable = getAttrValue(node, 'translatable')
-                if (translatable == 'false') {
-                    passingText = true
-                    continue
-                }
-
-                // name attr 없는 태그는 문제가 있는 것인데, 일단 스킵
-                const name = getAttrValue(node, 'name')
-                if (name == null) {
-                    passingText = true
-                    continue
-                }
-
-                // 번역이 없는 태그도 스킵
-                const transEntry = trans.find(name, null)
-                if (transEntry == null) {
-                    passingText = true
-                    continue
-                }
-                let values = transEntry.messages
-                if (Object.keys(values).length == 0) {
-                    passingText = true
-                    continue
-                }
-
-                let plFirstTextNode: XMLTextNode | null = null
-                let plLastTextNode: XMLTextNode | null = null
-                const plurals = (node as XMLTagNode).plurals
-                if (isTextNode(plurals[0])) {
-                    plFirstTextNode = plurals[0]
-                }
-                if (isTextNode(plurals[plurals.length - 1])) {
-                    plLastTextNode = plurals[plurals.length - 1] as XMLTextNode
-                }
-
-                let itemNode = findFirstTagNode(plurals, 'item', {quantity: 'other'})
-                if (itemNode == null) {
-                    itemNode = findFirstTagNode(plurals, 'item')
-                }
-                if (itemNode == null) {
-                    passingText = true
-                    continue
-                }
-
-                const dstPlurals: XMLNode[] = []
-                for (const [key, value] of Object.entries(transEntry.messages)) {
-                    if (plFirstTextNode != null) {
-                        dstPlurals.push({...plFirstTextNode})
-                    }
-                    const valueNode = createValueNode(itemNode, itemNode.item, value)
-                    dstPlurals.push({...itemNode, item: [valueNode], ':@': {'@_quantity': key}})
-                }
-                if (plLastTextNode != null) {
-                    dstPlurals.push({...plLastTextNode})
-                }
-                dstResources.push({...node, plurals: dstPlurals})
+    const dstResources: XMLNode[] = []
+    let passingText = false
+    for (const node of resNode.resources) {
+        if (isTextNode(node)) {
+            if (passingText) {
+                passingText = false
             } else {
                 dstResources.push(node)
             }
+            continue
         }
 
-        if (locale === defaultLocale) {
-            await writeXmlJson(builder, srcXmlJson, resDir, null)
+        // string 태그
+        if (isTagNode(node, 'string')) {
+            // translatable="false" 인 태그는 스킵
+            const translatable = getAttrValue(node, 'translatable')
+            if (translatable == 'false') {
+                passingText = true
+                continue
+            }
+
+            // name attr 없는 태그는 문제가 있는 것인데, 일단 스킵
+            const name = getAttrValue(node, 'name')
+            if (name == null) {
+                passingText = true
+                continue
+            }
+
+            // 번역이 없는 태그도 스킵
+            const transEntry = trans.find(name, null)
+            if (transEntry == null) {
+                passingText = true
+                continue
+            }
+            let value = transEntry.messages.other
+            if (!value) {
+                passingText = true
+                continue
+            }
+
+            const valueNode = createValueNode(node, node.string, value)
+            dstResources.push({...node, string: [valueNode]})
+        } else if (isTagNode(node, 'plurals')) {
+            // translatable="false" 인 태그는 스킵
+            const translatable = getAttrValue(node, 'translatable')
+            if (translatable == 'false') {
+                passingText = true
+                continue
+            }
+
+            // name attr 없는 태그는 문제가 있는 것인데, 일단 스킵
+            const name = getAttrValue(node, 'name')
+            if (name == null) {
+                passingText = true
+                continue
+            }
+
+            // 번역이 없는 태그도 스킵
+            const transEntry = trans.find(name, null)
+            if (transEntry == null) {
+                passingText = true
+                continue
+            }
+            let values = transEntry.messages
+            if (Object.keys(values).length == 0) {
+                passingText = true
+                continue
+            }
+
+            let plFirstTextNode: XMLTextNode | null = null
+            let plLastTextNode: XMLTextNode | null = null
+            const plurals = (node as XMLTagNode).plurals
+            if (isTextNode(plurals[0])) {
+                plFirstTextNode = plurals[0]
+            }
+            if (isTextNode(plurals[plurals.length - 1])) {
+                plLastTextNode = plurals[plurals.length - 1] as XMLTextNode
+            }
+
+            let itemNode = findFirstTagNode(plurals, 'item', {quantity: 'other'})
+            if (itemNode == null) {
+                itemNode = findFirstTagNode(plurals, 'item')
+            }
+            if (itemNode == null) {
+                passingText = true
+                continue
+            }
+
+            const dstPlurals: XMLNode[] = []
+            for (const [key, value] of Object.entries(transEntry.messages)) {
+                if (plFirstTextNode != null) {
+                    dstPlurals.push({...plFirstTextNode})
+                }
+                const valueNode = createValueNode(itemNode, itemNode.item, value)
+                dstPlurals.push({...itemNode, item: [valueNode], ':@': {'@_quantity': key}})
+            }
+            if (plLastTextNode != null) {
+                dstPlurals.push({...plLastTextNode})
+            }
+            dstResources.push({...node, plurals: dstPlurals})
+        } else {
+            dstResources.push(node)
         }
-
-        dstResNode.resources = dstResources
-
-        await writeXmlJson(builder, dstXmlJson, resDir, locale)
     }
+
+    dstResNode.resources = dstResources
+    return buildAndroidXml(builder, dstXmlJson)
 }
 
 function createValueNode(node: XMLTagNode, children: XMLNode[], value: string) {
@@ -183,7 +198,7 @@ function createValueNode(node: XMLTagNode, children: XMLNode[], value: string) {
     }
 }
 
-async function readXmlJson(parser: XMLParser, resDir: string, locale: string | null): Promise<XMLNode[]> {
+async function readXml(resDir: string, locale: string | null): Promise<string> {
     let targetPath: string
     if (locale == null) {
         targetPath = path.join(resDir, 'values', 'strings.xml')
@@ -191,13 +206,10 @@ async function readXmlJson(parser: XMLParser, resDir: string, locale: string | n
         targetPath = path.join(resDir, 'values-' + locale, 'strings.xml')
     }
 
-    const xml = await fs.readFile(targetPath, {encoding: 'utf-8'})
-    return await parseAndroidXml(parser, xml)
+    return await fs.readFile(targetPath, {encoding: 'utf-8'})
 }
 
-async function writeXmlJson(builder: XMLBuilder, xmlJson: XMLNode[], resDir: string, locale: string | null) {
-    const xml = buildAndroidXml(builder, xmlJson)
-
+async function writeXml(xml: string, resDir: string, locale: string | null) {
     let targetPath: string
     if (locale == null) {
         targetPath = path.join(resDir, 'values', 'strings.xml')
